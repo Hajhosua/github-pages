66363bfb4860cd51a826452faec9e6c63f6009de
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2025-05-03 11:20:09 -0500

    Actualicé App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>jhosuaaa</h1>
+      <h1>jhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
